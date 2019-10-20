c25723f21d4ad46b9746f8dc105f35ae62806d32
Boomaa23 <cmo93003@yahoo.com>
2019-10-19 19:58:12 -0700

    Functional with RPI
    Former-commit-id: bca3c6acbc0cd05eea97b0846b1f8b69e3c58b4a

--- src/org/rivierarobotics/enablekey/Main.java
+++ src/org/rivierarobotics/enablekey/Main.java
@@ -10,13 +10,11 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		DatagramSocket socket = new DatagramSocket();
-		InetAddress addr = InetAddress.getByName("10.58.18.2");
+		InetAddress addr = InetAddress.getByName("192.168.1.100");
 		
 		String strData = "28 00 01 04 10 03 08 0c 03 f5 f5 f5 02 00 00 08 0c 03 f5 f3 f1 02 00 00 08 0c 03 fd fd ff 02 00 00 08 0c 03 03 fe ff 02 00 00 0c 0c 00 20 00 00 00 00 02 ff ff ff ff 0c 0c 00 20 00 00 00 00 02 ff ff ff ff";
 		String strSplit[] = strData.split(" ");
@@ -27,22 +25,16 @@ public class Main {
 		
 		GpioController gpio = GpioFactory.getInstance();
 		GpioPinDigitalInput toggleOn = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-		toggleOn.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                for(int i = 0;event.getState().isHigh();i++) {
-        			data[1] = (byte) i;
-        			DatagramPacket packet = new DatagramPacket(data, 68, addr, 1110);
-        			try {
-						socket.send(packet);
-						Thread.sleep(20);
-					} catch (IOException | InterruptedException e) {
-						e.printStackTrace();
-					}
-        		}
-            }
-
-        });
-		socket.close();
+		int globalCounter = 0;
+		while(true) {
+			for(int i = globalCounter;toggleOn.isHigh();i++) {
+				data[1] = (byte) i;
+				DatagramPacket packet = new DatagramPacket(data, 68, addr, 1110);
+				socket.send(packet);
+				globalCounter = i;
+				Thread.sleep(20);
+			}
+			Thread.sleep(100);
+		}
 	}
 }
